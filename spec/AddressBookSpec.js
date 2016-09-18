52148a202eb27ffaa1f141789f8c02a75d929518
Your Name <you@example.com>
2016-09-18 10:26:42 -0700

    Where the files aaaat?

---
+++ spec/AddressBookSpec.js
@@ -0,0 +1,31 @@
+describe('Address Book', function() {
+	it('should be able to add a contact', function() {
+		var addressBook = new AddressBook(),
+			this Contact = new Contact();
+
+			beforeEach(function() {
+				addressBook = new AddressBook();
+				thisContact = new Contact();
+			});
+
+			addressBook.addContact(thisContact);
+
+			expect(addressBook.getContact(0)).toBe(thisContact);
+		});
+
+	it('should be able to add a contact', function() {
+		addressBook.addContact(thisContact);
+
+		expect(addressBook.getContact(0)).toBe(thisContact);
+	});
+
+	it('should be able to delete a contact', function() {
+		var addressBook = new AddressBook(),
+			thisContact = new Contact();
+
+		addressBook.addContact(thisContact);
+		addressBook.deleteContact(0);
+
+		expect(addressBook.getContact(0)).not.toBeDefined();
+	});
+});
